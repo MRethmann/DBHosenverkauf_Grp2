00b83dce8fad51bf4c40d2a6c4463445dbbba4b5
Staraptoah <luca.schwarte2000@gmail.com>
2020-12-28 12:54:39 +0100

    by Luca sollte überprüft werden bin nicht der schlauste so
    Patrick sinkt

---
+++ src/Lieferanten_zu_Produktstamm.sql
@@ -0,0 +1,13 @@
+USE hosenfabrik;
+
+DROP TABLE IF EXISTS Lieferanten_zu_Produktstamm;
+
+show fields from Lieferanten_zu_Produktstamm;
+
+DROP TABLE IF EXISTS Lieferanten_zu_Produktstamm;
+CREATE TABLE Lieferanten_zu_Produktstamm
+(
+    LieferantenID CHAR(10) NOT NULL,
+    ProduktID CHAR(10) NOT NULL,
+    PRIMARY KEY (LieferantenID, ProduktID)
+);

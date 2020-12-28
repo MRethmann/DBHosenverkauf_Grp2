94e7d8d5428bce55c627ed3c67e69d83eeea12cf
Tina Meiners <tina.meiners@hs-osnabrueck.de>
2020-12-28 13:18:33 +0100

    Shit

--- src/Produktionsmaterial.sql
+++ src/Produktionsmaterial.sql
@@ -7,15 +7,15 @@ show fields from Produktionsmaterial;
 DROP TABLE IF EXISTS Produktionsmaterial;
 CREATE TABLE Produktionsmaterial
 (
-    UntergruppeID INT NOT NULL ;
-    ObergruppeID INT NOT NULL ;
-    Menge Decimal(9,2) ;
-    Ursprungsland VARCHAR (255);
-    Bezeichnung_Obergruppe VARCHAR (255);
-    Bezeichnung_Untergruppe VARCHAR (255);
-    FarbID INT;
-    MerkmalID INT;
-    Primary Key (UntergruppeID,ObergruppeID);
+    UntergruppeID INT NOT NULL,
+    ObergruppeID INT NOT NULL,
+    Menge Decimal(9,2),
+    Ursprungsland VARCHAR (255),
+    Bezeichnung_Obergruppe VARCHAR (255),
+    Bezeichnung_Untergruppe VARCHAR (255),
+    FarbID INT,
+    MerkmalID INT,
+    Primary Key (UntergruppeID,ObergruppeID)
 
 
-)
+);

3d3da97f1d767f8d71b536ae54cd3f7047336389
DK1829 <hanekampdirk2000@gmail,com>
2021-01-06 13:07:49 +0100

    Create Befehle

--- src/Telefonnummern.sql
+++ src/Telefonnummern.sql
@@ -8,8 +8,17 @@ CREATE TABLE IF NOT EXISTS Telefonnummern
     PRIMARY KEY (ReferenzID, Telefonnummer)
 );
 
+ALTER TABLE Telefonnummern
+    ADD CONSTRAINT FOREIGN KEY(ReferenzID) REFERENCES Personalstamm(PersonalID)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE;
 
+ALTER TABLE Telefonnummern
+    ADD CONSTRAINT FOREIGN KEY(ReferenzID) REFERENCES Lieferantenstamm(LieferantenID)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE;
 
-
-
-
+ALTER TABLE Telefonnummern
+    ADD CONSTRAINT FOREIGN KEY(ReferenzID) REFERENCES Kundenstamm(KundenID)
+        ON DELETE RESTRICT
+        ON UPDATE CASCADE;

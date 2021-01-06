2addd2d9908b5b28f2ab65e15d1b08bff85c4315
Patrick <patrick.benten@yahoo.de>
2021-01-06 13:13:04 +0100

    Einfach die Hübsche datenbank

--- src/Telefonnummern.sql
+++ src/Telefonnummern.sql
@@ -3,23 +3,26 @@ USE hosenfabrik;
 DROP TABLE IF EXISTS Telefonnummern;
 CREATE TABLE IF NOT EXISTS Telefonnummern
 (
-    ReferenzID CHAR(10) NOT NULL,
+    ReferenzKunde CHAR(10) NOT NULL,
+    ReferenzPersonal CHAR(10) NOT NULL,
+    ReferenzLieferant CHAR(10) NOT NULL,
+    TelefonnummerID Integer auto_increment,
     Telefonnummer VARCHAR(20),
-    PRIMARY KEY (ReferenzID, Telefonnummer)
+    PRIMARY KEY (TelefonnummerID, Telefonnummer)
 );
 
 
 ALTER TABLE Telefonnummern
-    ADD CONSTRAINT FOREIGN KEY(ReferenzID) REFERENCES Personalstamm(PersonalID)
+    ADD CONSTRAINT FOREIGN KEY(ReferenzPersonal) REFERENCES Personalstamm(PersonalID)
         ON DELETE RESTRICT
         ON UPDATE CASCADE;
 
 ALTER TABLE Telefonnummern
-    ADD CONSTRAINT FOREIGN KEY(ReferenzID) REFERENCES Lieferantenstamm(LieferantenID)
+    ADD CONSTRAINT FOREIGN KEY(ReferenzLieferant) REFERENCES Lieferantenstamm(LieferantenID)
         ON DELETE RESTRICT
         ON UPDATE CASCADE;
 
 ALTER TABLE Telefonnummern
-    ADD CONSTRAINT FOREIGN KEY(ReferenzID) REFERENCES Kundenstamm(KundenID)
+    ADD CONSTRAINT FOREIGN KEY(ReferenzKunde) REFERENCES Kundenstamm(KundenID)
         ON DELETE RESTRICT
         ON UPDATE CASCADE;

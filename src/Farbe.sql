97b6810d32ba3a9c9a98ac916bbf19c9db1cc3f9
Maurice Rethmann <maurice.rethmann@hs-osnabrueck.de>
2021-01-13 14:37:59 +0100

    PERFEKTO

--- src/Farbe.sql
+++ src/Farbe.sql
@@ -3,8 +3,9 @@ USE hosenfabrik;
 DROP TABLE IF EXISTS Farbe;
 CREATE TABLE Farbe
 (
-    FarbID  INTEGER NOT NULL,
-    Farbe   VARCHAR (255),
+    FarbID      INTEGER NOT NULL,
+    Farbe       VARCHAR (255),
+
     PRIMARY KEY (FarbID)
 );
 
